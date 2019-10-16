08b4b7072b4967bfb41ef9ae0d4f69586c805f87
Parvez Ahmmed <56607383+parvez2083@users.noreply.github.com>
2019-10-16 18:51:41 -0400

    Update model4projC.py

--- model4projC.py
+++ model4projC.py
@@ -5,13 +5,13 @@ import data4projC as dt
 # Network parameters.
 conv1_filters = 8       # number of filters for 1st conv layer.
 conv1_size = [21, 11]   # size of filters for 1st conv layer.
-pool1_size = [2, 3]
+pool1_size = [2, 3]     # size of 1st maxpool layer.
 conv2_filters = 16      # number of filters for 2nd conv layer.
 conv2_size = 7          # size of filters for 2nd conv layer.
-pool2_size = [2, 2]
-conv3_filters = 32      # number of filters for 2nd conv layer.
-conv3_size = 3          # size of filters for 2nd conv layer.
-pool3_size = [2, 2]
+pool2_size = [2, 2]     # size of 2nd maxpool layer.
+conv3_filters = 32      # number of filters for 3rd conv layer.
+conv3_size = 3          # size of filters for 3rd conv layer.
+pool3_size = [2, 2]     # size of 3rd maxpool layer.
 #fc1_units = 128        # number of neurons for 1st fully-connected layer.
 #fc2_units = 64         # number of neurons for 2nd fully-connected layer.
 
