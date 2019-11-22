f0650df5a026d0ab9251d068de5a9b0d07ae2671
Kunal Narang <knarang@wolftech.ad.ncsu.edu>
2019-11-22 11:15:39 -0500

    Added VGG16 for clustering, needs debugging

--- clustering.py
+++ clustering.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os
 import cv2
 import matplotlib.pyplot as plt
@@ -8,20 +10,19 @@ import numpy as np
 import tensorflow as tf
 
 
-root_dir = os.path.abspath('.')
+root_dir = os.path.abspath('./')
 # Set the directory of the training data
 train_dir = os.path.join(root_dir, 'Training')
 
-
-
 train_files = pd.read_csv(os.path.join(root_dir, 'train_files.csv'))
-print(train_files)
 
 temp = []
-
+n=0
 for img_name in train_files.file_name:
     image_path = os.path.join(train_dir, img_name)
-    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE).flatten()
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    # .flatten()
+    # print(n)
     # print(type(img))
     # print(img.shape)
     # cv2.imshow('image', img)
@@ -29,62 +30,133 @@ for img_name in train_files.file_name:
     # cv2.destroyAllWindows()
     img = img.astype('float32')
     temp.append(img)
+    n+=1
 
-train_x = np.stack(temp)
+X_train = np.stack(temp)
 
-train_x /= 255.0
-train_x = train_x.reshape(-1, 307200).astype('float32')
-
-train_y = train_files.annotation.values
-
-split_size = int(train_x.shape[0]*0.7)
-
-train_x, val_x = train_x[:split_size], train_x[split_size:]
-train_y, val_y = train_y[:split_size], train_y[split_size:]
-
-km = KMeans(n_jobs=-1, n_clusters=5, n_init=20)
-
-# km.fit(train_x)
-#
-# pred = km.predict(val_x)
-#
-# score = normalized_mutual_info_score(val_y, pred)
-# print(score)
+# change this for different shapes
+X_train = X_train.reshape(n, 480, 640, 1)
 
 
-# this is our input placeholder
-input_img = tf.keras.Input(shape=(307200,))
+# train_x /= 255.0
+# train_x = train_x.reshape(-1, 307200).astype('float32')
 
-# "encoded" is the encoded representation of the input
-encoded = tf.keras.layers.Dense(500, activation='relu')(input_img)
-encoded = tf.keras.layers.Dense(500, activation='relu')(encoded)
-encoded = tf.keras.layers.Dense(2000, activation='relu')(encoded)
-encoded = tf.keras.layers.Dense(10, activation='sigmoid')(encoded)
+y_train = train_files.annotation.values
 
-# "decoded" is the lossy reconstruction of the input
-decoded = tf.keras.layers.Dense(2000, activation='relu')(encoded)
-decoded = tf.keras.layers.Dense(500, activation='relu')(decoded)
-decoded = tf.keras.layers.Dense(500, activation='relu')(decoded)
-decoded = tf.keras.layers.Dense(307200)(decoded)
+# Use first 700 examples or training, 100 for testing, 96 for validation
+train_size = 700
+test_size = 100
 
-# this model maps an input to its reconstruction
-autoencoder = tf.keras.Model(input_img, decoded)
-autoencoder.summary()
+X_train, X_test = X_train[:train_size], X_train[train_size:]
+y_train, y_test = y_train[:train_size], y_train[train_size:]
 
-#  this model maps an input to its encoded representation
-encoder = tf.keras.Model(input_img, encoded)
+# X_test, X_val = X_test[:test_size], X_test[test_size:]
+# y_test, y_val = y_test[:test_size], y_test[test_size:]
 
-autoencoder.compile(optimizer='adam', loss='mse')
+print(X_train.shape)
+print(X_test.shape)
+# print(X_val.shape)
 
-train_history = autoencoder.fit(train_x, train_x, epochs=10, batch_size=16, validation_data=(val_x, val_x))
 
-pred_auto_train = encoder.predict(train_x)
-pred_auto = encoder.predict(val_x)
 
-km.fit(pred_auto_train)
-pred = km.predict(pred_auto)
+with tf.device('/device:GPU:0'):
 
-score = normalized_mutual_info_score(val_y, pred)
-print(score)
-print(km.labels_)
+    vgg16_model = tf.keras.applications.VGG16(include_top=False, 
+                                            weights=None, 
+                                            input_tensor=None, 
+                                            input_shape=(480, 640, 1), 
+                                            pooling=None, 
+                                            classes=5)
 
+    vgg16_model.summary()          
+
+
+
+    # print(vgg16_model.layers[-1].output_shape)                             
+
+    # model = tf.keras.Sequential()
+
+    # # Remove the prediction layer and add to new model
+    # for layer in vgg16_model.layers[:-1]: 
+    #     model.add(layer)    
+
+    # # Freeze the layers 
+    # for layer in model.layers:
+    #     layer.trainable = False
+
+    # # Add 'softmax' instead of earlier 'prediction' layer.
+    # model.add(tf.keras.layers.Dense(5, activation='softmax'))
+
+    y_pred = vgg16_model.predict(X_train)
+
+
+
+
+
+    # model.summary()
+
+    # model.compile(optimizer=tf.keras.optimizers.Adam(), 
+    #               loss=tf.keras.losses.sparse_categorical_crossentropy,
+    #               metrics=["accuracy"])
+
+
+
+
+    # # prevents changing the weights and freezes them
+    # vgg16_model.trainable=False
+    # global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+    # prediction_layer = tf.keras.layers.Dense(5,activation='softmax')
+
+    # model = tf.keras.Sequential([
+    #   vgg16_model,
+    #   global_average_layer,
+    #   prediction_layer
+    # ])
+
+
+
+    print(y_pred.shape)
+
+
+
+
+
+    # Do k-means clustering on the VGG_16 output
+    km = KMeans(n_jobs=-1, n_clusters=5, n_init=20)
+
+
+
+    # this is our input placeholder
+    input_img = tf.keras.Input(shape=(y_pred.shape))
+
+    # "encoded" is the encoded representation of the input
+    encoded = tf.keras.layers.Dense(500, activation='relu')(input_img)
+    encoded = tf.keras.layers.Dense(500, activation='relu')(encoded)
+    encoded = tf.keras.layers.Dense(2000, activation='relu')(encoded)
+    encoded = tf.keras.layers.Dense(10, activation='sigmoid')(encoded)
+
+    # "decoded" is the lossy reconstruction of the input
+    decoded = tf.keras.layers.Dense(2000, activation='relu')(encoded)
+    decoded = tf.keras.layers.Dense(500, activation='relu')(decoded)
+    decoded = tf.keras.layers.Dense(500, activation='relu')(decoded)
+    decoded = tf.keras.layers.Dense(307200)(decoded)
+
+    # this model maps an input to its reconstruction
+    autoencoder = tf.keras.Model(input_img, decoded)
+    autoencoder.summary()
+
+    #  this model maps an input to its encoded representation
+    encoder = tf.keras.Model(input_img, encoded)
+
+    autoencoder.compile(optimizer='adam', loss='mse')
+
+    train_history = autoencoder.fit(y_pred, y_pred, epochs=10, batch_size=16)
+
+    pred_auto_train = encoder.predict(X_train)
+    pred_auto = encoder.predict(X_test)
+
+    km.fit(pred_auto_train)
+    pred = km.predict(pred_auto)
+
+    score = normalized_mutual_info_score(y_test, pred)
+    print(score)
