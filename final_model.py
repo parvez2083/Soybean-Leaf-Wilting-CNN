69483ad42f3ff8054a30c93b09b1a0403ba6e474
Kunal Narang <kunalnarang1994@gmail.com>
2019-11-30 13:46:54 -0500

    Added predictions and best model

---
+++ final_model.py
@@ -0,0 +1,247 @@
+# -*- coding: utf-8 -*-
+import os
+import cv2
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+
+
+"""
+Two different methods for reading training and testing images since train_files.csv does not preserve order of files in the training images folder
+"""
+
+
+def read_training_images(train_files, train_dir):
+    """
+    A method that reads the training image files in a given directory and returns the list of all images as numpy arrays
+    """
+
+    images=[]
+    n=0
+    for img_name in train_files.file_name:
+        image_path = os.path.join(train_dir, img_name)
+        img = cv2.imread(image_path)
+        # img = np.expand_dims(cv2.resize(img, dsize=(COLS, ROWS), interpolation=cv2.INTER_CUBIC), axis=2)
+        # .flatten()
+        # print(n)
+        # print(type(img))
+        # print(img.shape)
+        # cv2.imshow('image', img)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        img = img.astype('float32')
+        images.append(img)
+        n+=1
+    print("Number of training images read = {}".format(n))
+
+    return images
+
+
+
+def read_testing_images(directory):
+    """
+    A method that reads the test image files in a given directory and returns the list of all images as numpy arrays
+    """
+    files = os.listdir(directory)
+
+    images=[]
+    m=0
+    for fil in files:
+        image_path = os.path.join(directory, fil)
+        img = cv2.imread(image_path)
+        # img = np.expand_dims(cv2.resize(img, dsize=(COLS, ROWS), interpolation=cv2.INTER_CUBIC), axis=2)
+        img = img.astype('float32')
+        images.append(img)
+        m+=1
+    print("Number of testing images read = {}".format(m))
+
+    return images
+
+
+
+def custom_vgg16(layer1_size=512, layer2_size=512, layer3_size=256, dropout=0.3, activation="relu", cols=480, rows=640, channel=3):
+    """
+    A model that generates a custom VGG16 model by adding dense layer to its bottom.
+    The top layers from VGG16 were not used. The extracted VGG16 layers were used for
+    feature representation and hence were not made trainable (frozen).
+    This function retuns a custom VGG16 model with all the layers added. 
+    """
+
+    
+    # initialize the VGG16 model from the keras library
+    vgg16_model = tf.keras.applications.VGG16(include_top=False,
+                                              weights='imagenet',
+                                              input_tensor=None,
+                                              input_shape=(cols, rows, channel),
+                                              pooling=None,
+                                              classes=5)
+
+    model = tf.keras.Sequential()
+
+    for layer in vgg16_model.layers[:-1]: 
+        model.add(layer)
+
+
+    # Freeze the layers
+    for layer in model.layers:
+        layer.trainable = False
+
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(layer1_size, activation=activation))
+    model.add(tf.keras.layers.Dropout(dropout))
+    model.add(tf.keras.layers.Dense(layer2_size, activation=activation))
+    model.add(tf.keras.layers.Dropout(dropout))
+    model.add(tf.keras.layers.Dense(layer3_size, activation=activation))
+    model.add(tf.keras.layers.Dropout(dropout))
+    # model.add(tf.keras.layers.Dense(layer4_size, activation=activation))
+    # model.add(tf.keras.layers.Dropout(dropout))
+
+    # Add 'softmax' instead of earlier 'prediction' layer.
+    model.add(tf.keras.layers.Dense(5, activation='softmax'))
+
+    return model
+
+
+
+def generate_plots(model_history, epochs):
+    """
+    A method that takes the model history of a trained model and plots its:
+    1. Training accuracy
+    2. Training loss
+    3. Validation accuracy
+    4. Validation loss
+    """
+    acc = model_history.history['acc']
+    val_acc = model_history.history['val_acc']
+    loss = model_history.history['loss']
+    val_loss = model_history.history['val_loss']
+
+
+    plt.figure(1)
+    plt.suptitle('Accuracy learning curve', fontsize=20)
+    plt.xlabel('epochs', fontsize=14)
+    plt.ylabel('accuracy', fontsize=14)
+    plt.plot(acc, label='training accuracy')
+    plt.plot(val_acc, label='validation accuracy')
+    plt.xticks(np.arange(0, epochs, epochs/10))
+    plt.legend(loc="lower right")
+    plt.savefig("accuracy.png", dpi=300)
+
+    plt.figure(2)
+    plt.suptitle('Loss learning curve', fontsize=20)
+    plt.xlabel('epochs', fontsize=14)
+    plt.ylabel('loss', fontsize=14)
+    plt.plot(loss, label='training loss')
+    plt.plot(val_loss, label='validation loss')
+    plt.xticks(np.arange(0, epochs, epochs/10))
+    plt.legend(loc="upper right")
+    plt.savefig("loss.png", dpi=300)
+
+
+
+def best_results(model_history):
+    """
+    A method that prints the best validation accuracy and loss to a best_results.txt file.
+    """
+    acc = model_history.history['acc']
+    val_acc = model_history.history['val_acc']
+    loss = model_history.history['loss']
+    val_loss = model_history.history['val_loss']
+
+    
+    f = open("best_results.txt", "w+")
+    f.write("Training accuracy = {}\n".format(acc))
+    f.write("----------------------\n")
+    f.write("Training loss = {}\n".format(loss))
+    f.write("----------------------\n")
+    f.write("Validation accuracy = {}\n".format(val_acc))
+    f.write("----------------------\n")
+    f.write("Validation loss = {}\n".format(val_loss))
+    f.write("----------------------\n")
+    f.write("----------------------\n\n")
+    f.close()
+
+
+
+
+def main():
+    # Initialize the directories
+    root_dir = os.path.abspath('./')
+    train_dir = os.path.join(root_dir, 'Training')
+    test_dir = os.path.join(root_dir, 'Project_C2_Testing')
+
+
+    train_files = pd.read_csv(os.path.join(root_dir, 'train_files.csv'))
+
+    # set constants
+    COLS, ROWS, CHANNEL = 480, 640, 3
+
+    # training constants
+    EPOCHS = 30
+    BATCH_SIZE = 16
+
+    # read the training and testing images
+    training_images = read_training_images(train_files, train_dir)
+    testing_images = read_testing_images(test_dir)
+
+
+    X_train = np.stack(training_images)
+    y_train = train_files.annotation.values
+    X_test = np.stack(testing_images)
+
+
+    # Split the training data in training and validation sets
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=None, shuffle=True, stratify=y_train)
+
+    print("Shape of training set: {}".format(X_train.shape))
+    print("Shape of validation set: {}".format(X_val.shape))
+    print("Shape of testing set: {}".format(X_test.shape))
+
+
+    print("-------------------")
+    print("Compiling model")
+    print("-------------------")
+
+    # initialize the model
+    model = custom_vgg16(512, 512, 256, 0.4, "relu", COLS, ROWS, CHANNEL)
+
+    model.summary()
+
+    model.compile(loss=tf.keras.losses.sparse_categorical_crossentropy,
+                    optimizer=tf.keras.optimizers.Adam(),
+                    metrics=['accuracy'])
+
+
+    print("-------------------")
+    print("Training model")
+    print("-------------------")
+
+    with tf.device('/device:GPU:0'):
+        history = model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(X_val, y_val))
+    
+
+    predictions = model.predict(X_test, batch_size=16)
+
+    generate_plots(history, EPOCHS)
+
+    best_results(history)
+
+    test_labels = np.array([np.argmax(prediction) for prediction in predictions])
+    np.savetxt("predictions.csv", test_labels, fmt="%d", delimiter=",")
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
--- models.py
+++ models.py
@@ -12,15 +12,34 @@ from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
 
+# Initialize the directories
 root_dir = os.path.abspath('./')
-# root_dir = os.path.abspath('./drive/My Drive/C1/Project_C1/')
-# Set the directory of the training data
 train_dir = os.path.join(root_dir, 'Training')
+test_dir = os.path.join(root_dir, 'Project_C2_Testing')
+
 
 train_files = pd.read_csv(os.path.join(root_dir, 'train_files.csv'))
 
 # set constants
 COLS, ROWS, CHANNEL = 480, 640, 3
+
+
+def read_files(directory):
+    files = os.listdir(directory)
+
+    images=[]
+    m=0
+    for fil in files:
+        image_path = os.path.join(directory, fil)
+        img = cv2.imread(image_path)
+        img = img.astype('float32')
+        images.append(img)
+        m+=1
+    print("m = {}".format(m))
+
+    return images
+
+
 
 temp = []
 n=0
@@ -39,10 +58,22 @@ for img_name in train_files.file_name:
     temp.append(img)
     n+=1
 
-X_train = np.stack(temp)
 
+
+X_train = np.stack(temp)
 y_train = train_files.annotation.values
 
+
+testing_images = read_files(test_dir)
+X_test = np.stack(testing_images)
+
+
+print(X_train.shape)
+print(y_train.shape)
+print(X_test.shape)
+
+
+# Split the training data in training and validation sets
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=1, shuffle=True, stratify=y_train)
 
 
@@ -67,6 +98,22 @@ X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.
 #                                             input_shape=(COLS, ROWS, CHANNEL),
 #                                             pooling=None,
 #                                             classes=5)  ]
+
+
+def create_model(activation='relu', dropout=0.4,optimizer='adam',neurons=64): 
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Conv2D(128, kernel_size=3, padding='same', activation=activation, input_shape=(COLS, ROWS, CHANNEL)))
+    model.add(tf.keras.layers.MaxPool2D(pool_size=2))
+    model.add(tf.keras.layers.Dropout(dropout))
+    model.add(tf.keras.layers.Conv2D(128, kernel_size=3, padding='same', activation=activation))
+    model.add(tf.keras.layers.MaxPool2D(pool_size=2))
+    model.add(tf.keras.layers.Dropout(dropout))
+
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(256, activation=activation))
+    model.add(tf.keras.layers.Dense(256, activation=activation))
+    model.add(tf.keras.layers.Dense(5, activation='softmax'))
+    return model
 
 
 def custom_vgg16(layer1_size, layer2_size, layer3_size, dropout, activation):
@@ -98,23 +145,21 @@ def custom_vgg16(layer1_size, layer2_size, layer3_size, dropout, activation):
     model.add(tf.keras.layers.Dropout(dropout))
     # model.add(tf.keras.layers.Dense(layer4_size, activation=activation))
     # model.add(tf.keras.layers.Dropout(dropout))
-    model.add(tf.keras.layers.Dense(5, activation='softmax'))
 
     # Add 'softmax' instead of earlier 'prediction' layer.
-
+    model.add(tf.keras.layers.Dense(5, activation='softmax'))
 
     return model
 
-# +
-# Hyperparams
 
-layer1 = [1024]
+# Hyperparams
+layer1 = [512]
 layer2 = [512]
-layer3 = [512]
+layer3 = [256]
 # layer3 = [256, 512]
 # layer4 = [256, 512] 
 
-dropout = [0.4]
+dropout = [0.3]
 activation = ["relu"]
 
 EPOCHS = 30
@@ -202,60 +247,6 @@ for l1 in layer1:
             
 
 f.close()                    
-# -
-
-
-
-# +
-# model_names = ["Densenet", "Resnet50", "Mobilenetv2"]
-# model_names = ["VGG16 custom"]
-
-# +
-
-# for i, mdl in enumerate(models):
-
-#     # initialize the model
-#     model = mdl
-
-#     model.summary()
-#     model.compile(loss=tf.keras.losses.sparse_categorical_crossentropy,
-#                            optimizer=tf.keras.optimizers.Adam(),
-#                            metrics=['accuracy'])
-
-
-#     print("-------------------")
-#     print("Training {}".format(model_names[i]))
-#     print("-------------------")
-
-
-#     with tf.device('/device:GPU:0'):
-
-#         history = model.fit(X_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=(X_val, y_val))
-
-#         # acc = accuracy_score(y_test, y_pred)
-#         # pre = precision_score(y_test, y_pred, average='micro')
-#         # rec = recall_score(y_test, y_pred, average='micro')
-#         # f1 = f1_score(y_test, y_pred, average='micro')
-#         #
-#         # print('Accuracy: {}, Precision: {}, Recall: {}, F1 Score: {}'.format(acc, pre, rec, f1))
-
-#         acc = history.history['acc']
-#         val_acc = history.history['val_acc']
-#         loss = history.history['loss']
-#         val_loss = history.history['val_loss']
-
-
-#         plt.figure(i)
-#         plt.suptitle('Peformance', fontsize=20)
-#         plt.xlabel('epochs', fontsize=14)
-#         plt.ylabel('performance', fontsize=14)
-#         plt.plot(acc)
-#         plt.plot(val_acc)
-#         plt.plot(loss)
-#         plt.plot(val_loss)
-#         plt.xticks(np.arange(0, EPOCHS, EPOCHS/10))
-#         plt.savefig("./performance/{}_performance.png".format(model_names[i]), dpi=500)
-# -
 
 
 
